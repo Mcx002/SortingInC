5515b54313a95000ae332c0eaef332f3a6a6fdde
Muchlish Choeruddin <muchlishchoeruddin96@gmail.com>
2020-10-10 07:14:39 +0700

    add comment for certain line

--- main.c
+++ main.c
@@ -2,23 +2,34 @@
 #include <stdlib.h>
 #include <conio.h>
 
+/* struct itu untuk membuat tipe data, jadi disini kita membuat tipe data Mahasiswa*/
 struct Mahasiswa{
     int npm;
     int nilai;
 };
-
+/* trus dipake disini,
+karena kita akan menginput 10 mahasiswa dalam program kita,
+karena mereka punya data yang sama, biar gampang pengolahannya kita buat tipe datanya  */
 struct Mahasiswa mahasiswa[10],sortingMahasiswa[10];
+
+/* selected menu dibawa ini untuk menu yang nnti dipilih user */
 int selectedMenu = -2;
 int selectedMenuSorting = -2;
+
+/* kenapa nilainya semua -2, lupa:v. pokonya kayak buat basenya gitu lah/defaultnya */
 int cariNpm = -2;
 
+/* kita buat deklarasi fungsi diatas, nanti isinya di paling bawah */
 void inputNilaiMahasiswa();
 void cariNilaiMahasiswa();
 void sortingNilaiMahasiswaASC();
 void sortingNilaiMahasiswaDESC();
 
+/* kenapa dibuat fungsi, biar di int main() nya gk acak acakan banget, walopun disini udh acak acakan sih:v */
+
 int main()
 {
+    /* pakai while selectedMenu!=4 biar looping terus sampe user input 4 di pilihan menu, yaitu exit*/
     while(selectedMenu!=4){
         system("cls");
         printf("Program data Mahasiswa\n");
@@ -33,8 +44,11 @@ int main()
         printf("Pilih menu: ");
         scanf("%d",&selectedMenu);
 
+        /* pake switch buat kondisi user pilih apa nih */
         switch(selectedMenu){
             case 1:
+                /* kalo pilih input nilai, runing fungsi input nilai mahasiswa yang ada dibawah dan udh
+                di deklarasiin diatas */
                 inputNilaiMahasiswa();
                 break;
             case 2:
@@ -67,10 +81,16 @@ int main()
     }
     return 0;
 }
+/* ini nih fungsi input tuh, ngejalanin kek gini */
 void inputNilaiMahasiswa(){
     for(int i=0; i<10; i++){
         printf("Mahasiswa absen %d\n",i+1);
         printf("NPM: ");
+        /* tuhkan lebih rapih kalo udh bikin tipedata mahasiswa mah
+        tinggal masuk ke array mahasiswa index ke berapa, trus masukin ke objek nya.
+        klo gk dibikin tipe data pasti bakal biki banyak array kan? hayyoh loh makin bingung dan acak"an
+        jadi itu lah fungsi bikin tipedata sendiri
+         */
         scanf("%d",&mahasiswa[i].npm);
         printf("Nilai: ");
         scanf("%d",&mahasiswa[i].nilai);
@@ -78,6 +98,7 @@ void inputNilaiMahasiswa(){
     printf("\nSuccess\n");
     printf("Press enter to continue");
     getch();
+    /* getch ini tuh buat nge holding sebentar lah, biar gk langsung nutup trus balik ke menu */
 }
 void cariNilaiMahasiswa(){
     printf("Cari Mahasiswa :");
@@ -115,9 +136,16 @@ void sortingNilaiMahasiswaASC(){
 
 void sortingNilaiMahasiswaDESC(){
     struct Mahasiswa temp;
+    /*
+    ini tuh sebenarnya buat ngalihin data aja si ke sortingmahasiswa, soalnya variable mahasiswa itu adalah ori datanya
+    jadi jangan dirubah" harusnya, biar keorian datanya terjaga:v
+    */
     for(int i=0; i<10;i++){
         sortingMahasiswa[i]=mahasiswa[i];
     }
+    /*
+    trus ini fungsi sortingannya
+    */
     for(int a=0; a<10-1; a++){
         for(int b=0; b<10-1; b++){
             if(sortingMahasiswa[b].nilai<sortingMahasiswa[b+1].nilai){
