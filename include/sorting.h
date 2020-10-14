6010ee2283fd9aeb5e613f9a552fe0e9c9bd1b4f
Muchlish Choeruddin <muchlishchoeruddin96@gmail.com>
2020-10-14 18:13:09 +0700

    next task

---
+++ include/sorting.h
@@ -0,0 +1,13 @@
+#ifdef FOO_DOT_H
+#define FOO_DOT_H
+
+void inputNilaiMahasiswa();
+void cariNilaiMahasiswa();
+void sortingNilaiMahasiswaASC();
+void sortingNilaiMahasiswaDESC();
+void sortingInsertionASC();
+void sortingInsertionDESC();
+void sortingSelectionASC();
+void sortingSelectionDESC();
+
+#endif // FOO_DOT_H
--- main.c
+++ main.c
@@ -2,59 +2,55 @@
 #include <stdlib.h>
 #include <conio.h>
 
-/* struct itu untuk membuat tipe data, jadi disini kita membuat tipe data Mahasiswa*/
 struct Mahasiswa{
     int npm;
     int nilai;
 };
-/* trus dipake disini,
-karena kita akan menginput 10 mahasiswa dalam program kita,
-karena mereka punya data yang sama, biar gampang pengolahannya kita buat tipe datanya  */
-struct Mahasiswa mahasiswa[10],sortingMahasiswa[10];
 
-/* selected menu dibawa ini untuk menu yang nnti dipilih user */
+struct Mahasiswa mahasiswa[10],sortingMahasiswa[10];
 int selectedMenu = -2;
 int selectedMenuSorting = -2;
-
-/* kenapa nilainya semua -2, lupa:v. pokonya kayak buat basenya gitu lah/defaultnya */
 int cariNpm = -2;
 
-/* kita buat deklarasi fungsi diatas, nanti isinya di paling bawah */
 void inputNilaiMahasiswa();
 void cariNilaiMahasiswa();
 void sortingNilaiMahasiswaASC();
 void sortingNilaiMahasiswaDESC();
-
-/* kenapa dibuat fungsi, biar di int main() nya gk acak acakan banget, walopun disini udh acak acakan sih:v */
+void sortingInsertionASC();
+void sortingInsertionDESC();
+void sortingSelectionASC();
+void sortingSelectionDESC();
 
 int main()
 {
-    /* pakai while selectedMenu!=4 biar looping terus sampe user input 4 di pilihan menu, yaitu exit*/
-    while(selectedMenu!=4){
+    while(selectedMenu!=7){
         system("cls");
         printf("Program data Mahasiswa\n");
         printf("Created by: Muchlish Choeruddin\n");
         printf("NPM: 0619204001\n\n");
         printf("Menu:\n");
         printf("    1. Input nilai mahasiswa\n");
-        printf("    2. Searching\n");
-        printf("    3. Sorting\n");
-        printf("    4. Exit\n\n");
+        printf("    2. Searching(linear search)\n");
+        printf("    3. Searching(binary search)\n");
+        printf("    4. Sorting(bubble sort)\n");
+        printf("    5. Sorting(insertion sort)\n");
+        printf("    6. Sorting(selection sort)\n");
+        printf("    7. Exit\n\n");
 
         printf("Pilih menu: ");
         scanf("%d",&selectedMenu);
 
-        /* pake switch buat kondisi user pilih apa nih */
         switch(selectedMenu){
             case 1:
-                /* kalo pilih input nilai, runing fungsi input nilai mahasiswa yang ada dibawah dan udh
-                di deklarasiin diatas */
                 inputNilaiMahasiswa();
                 break;
             case 2:
                 cariNilaiMahasiswa();
                 break;
             case 3:
+                cariNilaiMahasiswaBinary();
+                break;
+            case 4:
                 printf("Sorting mahasiswa berdasar:\n");
                 printf("    1. Besar ke kecil\n");
                 printf("    2. Kecil ke besar\n\n");
@@ -71,7 +67,41 @@ int main()
                 }
 
                 break;
-            case 4:
+            case 5:
+                printf("Sorting mahasiswa berdasar:\n");
+                printf("    1. Besar ke kecil\n");
+                printf("    2. Kecil ke besar\n\n");
+
+                printf("Pilih menu: ");
+                scanf("%d",&selectedMenuSorting);
+                if(selectedMenuSorting==1){
+                    sortingInsertionDESC();
+                }else if(selectedMenuSorting==2){
+                    sortingInsertionASC();
+                }else{
+                    puts("Invalid value\nPress <enter> to continue");
+                    getch();
+                }
+
+                break;
+            case 6:
+                printf("Sorting mahasiswa berdasar:\n");
+                printf("    1. Besar ke kecil\n");
+                printf("    2. Kecil ke besar\n\n");
+
+                printf("Pilih menu: ");
+                scanf("%d",&selectedMenuSorting);
+                if(selectedMenuSorting==1){
+                    sortingSelectionDESC();
+                }else if(selectedMenuSorting==2){
+                    sortingSelectionASC();
+                }else{
+                    puts("Invalid value\nPress <enter> to continue");
+                    getch();
+                }
+
+                break;
+            case 7:
                 break;
             default:
                 puts("Invalid value\nPress <enter> to continue");
@@ -81,16 +111,10 @@ int main()
     }
     return 0;
 }
-/* ini nih fungsi input tuh, ngejalanin kek gini */
 void inputNilaiMahasiswa(){
     for(int i=0; i<10; i++){
         printf("Mahasiswa absen %d\n",i+1);
         printf("NPM: ");
-        /* tuhkan lebih rapih kalo udh bikin tipedata mahasiswa mah
-        tinggal masuk ke array mahasiswa index ke berapa, trus masukin ke objek nya.
-        klo gk dibikin tipe data pasti bakal biki banyak array kan? hayyoh loh makin bingung dan acak"an
-        jadi itu lah fungsi bikin tipedata sendiri
-         */
         scanf("%d",&mahasiswa[i].npm);
         printf("Nilai: ");
         scanf("%d",&mahasiswa[i].nilai);
@@ -98,7 +122,6 @@ void inputNilaiMahasiswa(){
     printf("\nSuccess\n");
     printf("Press enter to continue");
     getch();
-    /* getch ini tuh buat nge holding sebentar lah, biar gk langsung nutup trus balik ke menu */
 }
 void cariNilaiMahasiswa(){
     printf("Cari Mahasiswa :");
@@ -107,6 +130,43 @@ void cariNilaiMahasiswa(){
         if(mahasiswa[i].npm == cariNpm){
             printf("Mahasiswa dengan npm -- %d --.\n Meraih nilai %d\n",cariNpm,mahasiswa[i].nilai);
             break;
+        }
+    }
+    puts("\nPress <enter> to continue");
+    getch();
+}
+void cariNilaiMahasiswaBinary(){
+
+    struct Mahasiswa temp;
+    for(int i=0; i<10;i++){
+        sortingMahasiswa[i]=mahasiswa[i];
+    }
+    for(int a=0; a<10; a++){
+        int indexMin = a;
+        for(int j=a; j<10;j++){
+            if(sortingMahasiswa[j].npm<sortingMahasiswa[indexMin].npm){
+                indexMin=j;
+            }
+        }
+        temp = sortingMahasiswa[a];
+        sortingMahasiswa[a] = sortingMahasiswa[indexMin];
+        sortingMahasiswa[indexMin] = temp;
+    }
+
+    printf("Cari Mahasiswa :");
+    scanf("%d",&cariNpm);
+
+    int left = 0;
+    int right = 9;
+    while(left<=right){
+        int mid = left+((right-left)/2);
+        if(sortingMahasiswa[mid].npm==cariNpm){
+            printf("Mahasiswa dengan npm -- %d --.\n Meraih nilai %d\n",cariNpm,sortingMahasiswa[mid].nilai);
+            break;
+        }else if(cariNpm<sortingMahasiswa[mid].npm){
+            right = mid-1;
+        }else{
+            left = mid+1;
         }
     }
     puts("\nPress <enter> to continue");
@@ -136,16 +196,9 @@ void sortingNilaiMahasiswaASC(){
 
 void sortingNilaiMahasiswaDESC(){
     struct Mahasiswa temp;
-    /*
-    ini tuh sebenarnya buat ngalihin data aja si ke sortingmahasiswa, soalnya variable mahasiswa itu adalah ori datanya
-    jadi jangan dirubah" harusnya, biar keorian datanya terjaga:v
-    */
     for(int i=0; i<10;i++){
         sortingMahasiswa[i]=mahasiswa[i];
     }
-    /*
-    trus ini fungsi sortingannya
-    */
     for(int a=0; a<10-1; a++){
         for(int b=0; b<10-1; b++){
             if(sortingMahasiswa[b].nilai<sortingMahasiswa[b+1].nilai){
@@ -154,6 +207,96 @@ void sortingNilaiMahasiswaDESC(){
                 sortingMahasiswa[b]=temp;
             }
         }
+    }
+    printf("NPM        | Nilai\n");
+    for(int i=0; i<10; i++){
+        printf("%d         | %d\n",sortingMahasiswa[i].npm,sortingMahasiswa[i].nilai);
+    }
+    puts("\nPress <enter> to continue");
+    getch();
+}
+
+void sortingInsertionASC(){
+    struct Mahasiswa temp;
+    for(int i=0; i<10;i++){
+        sortingMahasiswa[i]=mahasiswa[i];
+    }
+    for(int a=1; a<=10; a++){
+        int j = a;
+        while(j>0 && sortingMahasiswa[j-1].nilai>sortingMahasiswa[j].nilai){
+            temp=sortingMahasiswa[j];
+            sortingMahasiswa[j]=sortingMahasiswa[j-1];
+            sortingMahasiswa[j-1]=temp;
+            j--;
+        }
+    }
+    printf("NPM        | Nilai\n");
+    for(int i=0; i<10; i++){
+        printf("%d         | %d\n",sortingMahasiswa[i].npm,sortingMahasiswa[i].nilai);
+    }
+    puts("\nPress <enter> to continue");
+    getch();
+}
+
+void sortingInsertionDESC(){
+    struct Mahasiswa temp;
+    for(int i=0; i<10;i++){
+        sortingMahasiswa[i]=mahasiswa[i];
+    }
+    for(int a=1; a<=10; a++){
+        int j = a;
+        while(j>0 && sortingMahasiswa[j-1].nilai<sortingMahasiswa[j].nilai){
+            temp=sortingMahasiswa[j];
+            sortingMahasiswa[j]=sortingMahasiswa[j-1];
+            sortingMahasiswa[j-1]=temp;
+            j--;
+        }
+    }
+    printf("NPM        | Nilai\n");
+    for(int i=0; i<10; i++){
+        printf("%d         | %d\n",sortingMahasiswa[i].npm,sortingMahasiswa[i].nilai);
+    }
+    puts("\nPress <enter> to continue");
+    getch();
+}
+void sortingSelectionASC(){
+    struct Mahasiswa temp;
+    for(int i=0; i<10;i++){
+        sortingMahasiswa[i]=mahasiswa[i];
+    }
+    for(int a=0; a<10; a++){
+        int indexMin = a;
+        for(int j=a; j<10;j++){
+            if(sortingMahasiswa[j].nilai<sortingMahasiswa[indexMin].nilai){
+                indexMin=j;
+            }
+        }
+        temp = sortingMahasiswa[a];
+        sortingMahasiswa[a] = sortingMahasiswa[indexMin];
+        sortingMahasiswa[indexMin] = temp;
+    }
+    printf("NPM        | Nilai\n");
+    for(int i=0; i<10; i++){
+        printf("%d         | %d\n",sortingMahasiswa[i].npm,sortingMahasiswa[i].nilai);
+    }
+    puts("\nPress <enter> to continue");
+    getch();
+}
+void sortingSelectionDESC(){
+    struct Mahasiswa temp;
+    for(int i=0; i<10;i++){
+        sortingMahasiswa[i]=mahasiswa[i];
+    }
+    for(int a=0; a<10; a++){
+        int indexMin = a;
+        for(int j=a; j<10;j++){
+            if(sortingMahasiswa[j].nilai>sortingMahasiswa[indexMin].nilai){
+                indexMin=j;
+            }
+        }
+        temp = sortingMahasiswa[a];
+        sortingMahasiswa[a] = sortingMahasiswa[indexMin];
+        sortingMahasiswa[indexMin] = temp;
     }
     printf("NPM        | Nilai\n");
     for(int i=0; i<10; i++){
